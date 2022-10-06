c358c0d84ad5f3f5d0cb42958022968a683e95d1
minhphuc1112001 <minhphuc.nguyenpsa@gmail.com>
2022-10-06 20:07:19 +0700

    add

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,14 +4,19 @@
 #include <queue>
 #include <time.h>
 #include <cstring>
+#include <memory>
 #define max 1000
 
 using namespace std;
 
 fstream fin, fout;
 vector<int> graph[max];
+int matrix[max][max];
+vector<int> mark;
 bool visited[max];
 int n, k = 1, destination = 13;
+vector<int> result;
+int length_road = INT32_MAX;
 queue<int> g;
 
 void bfs()
@@ -29,24 +34,59 @@ void bfs()
             {
                 visited[v] = true;
                 if (visited[destination])
+
                     return;
                 g.push(v);
             }
         }
     }
 }
+int sum_of_mark()
+{
+    int res = 0;
+    for (int i = 0; i < (int)mark.size() - 1; i++)
+    {
+        res += matrix[mark[i]][mark[i + 1]];
+    }
+    return res;
+}
 
+void print_result_dfs()
+{
+    fout << "DFS" << endl;
+    fout << "Path : " << endl
+         << "1 => ";
+    for (int i = 0; i < result.size(); i++)
+        fout << result[i] << " => ";
+    fout << destination << endl;
+    fout << "Length : " << length_road + matrix[1][result[0]] + matrix[result[result.size() - 1]][destination] << endl;
+}
 void dfs(int vt)
 {
     visited[vt] = true;
-    if (visited[destination])
-        return;
+
     for (int i = 0; i < graph[vt].size(); i++)
     {
         int v = graph[vt][i];
         if (!visited[v])
         {
-            dfs(v);
+
+            if (v == destination)
+            {
+                int distance = sum_of_mark();
+                if (length_road > distance)
+                {
+                    length_road = distance;
+                    result = mark;
+                }
+            }
+            else
+            {
+                mark.push_back(v);
+                dfs(v);
+                visited[v] = false;
+                mark.pop_back();
+            }
         }
     }
 }
@@ -62,12 +102,15 @@ int main()
         fin >> x >> y >> z;
         graph[x].push_back(y);
         graph[y].push_back(x);
+        matrix[x][y] = z;
+        matrix[y][x] = z;
     }
 
     clock_t start, end;
 
     start = clock();
     dfs(1);
+    print_result_dfs();
     end = clock();
 
     double time_taken_dfs = double(end - start) / double(CLOCKS_PER_SEC);
